e50ae21556a6b6c365960017cb24ba0ad4bb8c4c
Sunnatjon Muratov <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-08-17 09:44:05 +0500

    Add files via upload

---
+++ rules.py
@@ -0,0 +1,12 @@
+def determine_winner(user_choice, computer_choice):
+    """
+    Determine the winner based on the user and computer choices.
+    """
+    if user_choice == computer_choice:
+        return "It's a tie!"
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+            (user_choice == "paper" and computer_choice == "rock") or \
+            (user_choice == "scissors" and computer_choice == "paper"):
+        return "You win!"
+    else:
+        return "You lose!"
