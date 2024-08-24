7988746fa52436cc1aa45cefcb2e3ee4292669bb
Sunnatjon Muratov <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-08-24 21:47:01 +0500

    Update game.py

--- game.py
+++ game.py
@@ -1,35 +1,27 @@
-from player import get_user_choice, get_computer_choice
-from rules import determine_winner
-from utils import ask_to_play_again
+from flask import Flask, render_template, request, jsonify
+from player import get_computer_choice
+from rules.py import determine_winner
+
+app = Flask(__name__)
 
 
-def play_game():
-    """
-    Main function to play the Rock, Paper, Scissors game
-    """
-    user_score = 0
-    computer_score = 0
-
-    while True:
-        user_choice = get_user_choice()
-        computer_choice = get_computer_choice()
-        print(f"\nYou chose: {user_choice}")
-        print(f"Computer chose: {computer_choice}")
-
-        result = determine_winner(user_choice, computer_choice)
-        print(result)
-
-        if result == "You win!":
-            user_score += 1
-        elif result == "You lose!":
-            computer_score += 1
-
-        print(f"\nScore: You {user_score} - {computer_score} Computer")
-
-        if not ask_to_play_again():
-            print("Thanks for playing!")
-            break
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 
-if __name__ == "__main__":
-    play_game()
+@app.route('/play', methods=['POST'])
+def play():
+    user_choice = request.json.get('choice')
+    computer_choice = get_computer_choice()
+    result = determine_winner(user_choice, computer_choice)
+
+    return jsonify({
+        'user_choice': user_choice,
+        'computer_choice': computer_choice,
+        'result': result
+    })
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
